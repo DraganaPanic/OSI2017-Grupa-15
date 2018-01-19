3e6e288cf9d5dec48b67d7af455774584afc74ff
Dobrovoljski <tanja.dobrovoljski@gmail.com>
2018-01-19 17:51:47 +0000

    izmijenjena klasa korisnik

--- Korsnik-klasa/Korsnik-klasa/korisnik.h
+++ Korsnik-klasa/Korsnik-klasa/korisnik.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <exception>
 #include <fstream>
 #include <cstdio>
 #include <tuple>
@@ -10,23 +9,25 @@ using namespace std;
 
 class Korisnik
 {
-	friend ostream &operator<<(ostream&, const Korisnik&) noexcept;
-	friend istream &operator>>(istream&, Korisnik&) noexcept;
+	friend ostream &operator<<(ostream&, const Korisnik&);
+	friend istream &operator >> (istream&, Korisnik&);
 public:
 	Korisnik() = default;
-	Korisnik(std::string, std::string, std::string, string)noexcept;
+	Korisnik(std::string, std::string, std::string, std::string);
 	/*std::string getIme() const noexcept;
 	std::string getPrezime() const noexcept;
 	std::string getPin() const noexcept;
 	string getKorisnickaGrupa() const noexcept;*/
-	Korisnik dodaj() noexcept(false);
-	void registracija(Korisnik*)noexcept(false);
-	void prijava(Korisnik*) noexcept(false);
-	~Korisnik() = default;
-
+	Korisnik dodaj();
+	void registracija(Korisnik*);
+	void prijava(Korisnik*);
+	virtual ~Korisnik() = default;
+	bool is_number(string& src);
 protected:
 	std::string Ime, Prezime, Pin, korisnickaGrupa;
 private:
 	const char* poruka1 = "Invalid PIN!\n";
 	const char* poruka2 = "This person already exist!\n";
 };
+
+

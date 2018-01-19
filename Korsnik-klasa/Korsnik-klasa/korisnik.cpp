b7a71513d728a100e9a45f13407e67fd7632eb3b
Dobrovoljski <tanja.dobrovoljski@gmail.com>
2018-01-19 18:15:24 +0000

    izmijenjen .cpp file

--- Korsnik-klasa/Korsnik-klasa/korisnik.cpp
+++ Korsnik-klasa/Korsnik-klasa/korisnik.cpp
@@ -1,7 +1,7 @@
-#include "Header.h"
+#include "korisnik.h"
 
 
-Korisnik::Korisnik(std::string ime, std::string prezime, std::string pin, string korisnickagrupa) noexcept : Ime(ime), Prezime(prezime), Pin(pin), korisnickaGrupa(korisnickagrupa)
+Korisnik::Korisnik(std::string ime, std::string prezime, std::string pin, string korisnickagrupa) : Ime(ime), Prezime(prezime), Pin(pin), korisnickaGrupa(korisnickagrupa)
 {
 }
 /*
@@ -24,7 +24,7 @@ string Korisnik::getKorisnickaGrupa() const noexcept
 return korisnickaGrupa;
 }*/
 
-ostream& operator <<(ostream& out, const Korisnik& korisnik) noexcept
+ostream& operator <<(ostream& out, const Korisnik& korisnik)
 {
 	out << korisnik.Ime << endl;
 	out << korisnik.Prezime << endl;
@@ -33,7 +33,7 @@ ostream& operator <<(ostream& out, const Korisnik& korisnik) noexcept
 	return out;
 }
 
-std::istream& operator >>(istream& in, Korisnik& korisnik) noexcept
+std::istream& operator >> (istream& in, Korisnik& korisnik)
 {
 	in >> korisnik.Ime;
 	in >> korisnik.Prezime;
@@ -42,7 +42,7 @@ std::istream& operator >>(istream& in, Korisnik& korisnik) noexcept
 	return in;
 }
 
-void Korisnik::registracija(Korisnik* p) noexcept(false)
+void Korisnik::registracija(Korisnik* p)
 {
 	if (p != 0) {
 		fstream dat("registrovani.txt", ios::app);
@@ -50,13 +50,13 @@ void Korisnik::registracija(Korisnik* p) noexcept(false)
 
 		if (dat)
 		{
-			cout << "otvorena je tebra";
+
 			string ime, prezime, pin, korgr;
 
 			while (dat >> ime >> prezime >> pin >> korgr)
 			{
 				if (p->Ime == ime && p->Prezime == prezime)
-					throw poruka2;
+					cout << "This person already exists!\n";
 			}
 
 
@@ -67,18 +67,47 @@ void Korisnik::registracija(Korisnik* p) noexcept(false)
 	}
 }
 
-Korisnik Korisnik::dodaj() noexcept (false)
+Korisnik Korisnik::dodaj()
 {
 	Korisnik p;
-	cout << "Ime: "; cin >> p.Ime; cout << endl;
-	cout << "Prezime: "; cin >> p.Prezime; cout << endl;
-	cout << "PIN: "; cin >> p.Pin; cout << endl;
-	cout << "Korisnicka grupa: "; cin >> p.korisnickaGrupa; cout << endl;
+	cout << "Ime: "; cin >> p.Ime;
+	cout << "Prezime: "; cin >> p.Prezime;
+	cout << "PIN: "; cin >> p.Pin;
 
-	if (size(p.Pin) < 4 || size(p.Pin) > 4)
+
+	if ((p.is_number(p.Pin) != 1) || (p.Pin.length() != 4))
 	{
-		throw poruka1;
+		do { cout << "PIN1 "; cin >> p.Pin; } while ((p.is_number(p.Pin) != 1) || (p.Pin.length() != 4));
+
 	}
-	else
-		return p;
+	cout << "Korisnicka grupa: "; cin >> p.korisnickaGrupa;
+
+	if ((p.korisnickaGrupa != "administrator") && (p.korisnickaGrupa != "analiticar"))
+	{
+		do { cout << "Korisnicka grupa "; cin >> p.korisnickaGrupa; } while ((p.korisnickaGrupa != "administrator") && (p.korisnickaGrupa != "analiticar"));
+	}
+
+	return p;
+}
+
+bool Korisnik::is_number(string& src)
+{
+	const char *start = src.c_str();
+
+
+	errno = 0;
+	char *end;
+	long string_value = strtol(start, &end, 10);
+
+	if (
+		errno == ERANGE ||                                 // Did we set off errno?
+		string_value > numeric_limits<int>::max() ||       // Is it too big?
+		string_value < numeric_limits<int>::min() ||       // Too small?
+		static_cast<unsigned>(end - start) != src.length() // Non-numerics?
+		)
+	{
+		return false;
+	}
+
+	return true;
 }
